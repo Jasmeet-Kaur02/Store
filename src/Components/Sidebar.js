28b1aa1b66f00d7c57dfdf3d777f361156e7afd6
JasmeetWillFlyx <jasskaur.dha@gmail.com>
2021-07-27 18:51:55 +0530

    added effect to sidebar items

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,20 +1,27 @@
 import React from "react";
-import ReactDom from "react-dom";
 import classnames from "classnames";
 import "../CSS/Sidebar.css";
 import { Divider } from "@material-ui/core";
 import dividerStyle from "../CSS/IconStyle";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
   const [show, setShow] = React.useState(false);
   const classes = dividerStyle();
+  const currPath = useLocation().pathname;
+  console.log(currPath);
 
   const line1 = classnames("line", { line1: show });
   const line2 = classnames("line", { line2: show });
   const line3 = classnames("line", { line3: show });
   const sidebar = classnames("sidebar", { open: show });
-  const sidebarRoot = document.getElementById("sidebar-root");
-  console.log(show);
+  const links = [
+    { name: "Home", path: "/", id: 1 },
+    { name: "My Cart", path: "/checkout", id: 2 },
+    { name: "Orders", path: "/order", id: 3 },
+    { name: "Favorites", path: "/favorite", id: 4 },
+    { name: "Profile", path: "/profile", id: 5 },
+  ];
 
   return (
     <>
@@ -28,27 +35,30 @@ const Sidebar = () => {
           <span className={line3}></span>
         </div>
       </section>
-      {ReactDom.createPortal(
-        <div className={sidebar}>
-          <div className="sidebarHeader">
-            <h3>PetsStore</h3>
-          </div>
-          <div className="sidebarBody">
-            <ul>
-              <li>Home</li>
-              <li>Profile</li>
-              <li>My Cart</li>
-              <li>Favorites</li>
-              <li>Orders</li>
-            </ul>
-          </div>
-          <Divider className={classes.divider} />
-          <ul className="sidebarFooter">
-            <li>Sign Out</li>
+
+      <div className={sidebar}>
+        <div className="sidebarHeader">
+          <h3>PetsStore</h3>
+        </div>
+        <div className="sidebarBody">
+          <ul>
+            {links.map((link) => (
+              <NavLink
+                key={link.id}
+                to={link.path}
+                className="link"
+                activeClassName={link.path === currPath ? "link-active" : null}
+              >
+                <li className="link-items">{link.name}</li>
+              </NavLink>
+            ))}
           </ul>
-        </div>,
-        sidebarRoot
-      )}
+        </div>
+        <Divider className={classes.divider} />
+        <ul className="sidebarFooter">
+          <li>Sign Out</li>
+        </ul>
+      </div>
     </>
   );
 };
